c64519be82194cb86d1f9cdcbdc1d10ed3ca5b46
Aleksander <aleksander.kedzierski@gmail.com>
2013-08-09 13:33:08 +0200

    Forgot to delete files in last commit

--- appify/src/pl/poznan/put/cs/ify/position/YTimerTrigger.java
+++
@@ -1,9 +0,0 @@
-package pl.poznan.put.cs.ify.position;
-
-import pl.poznan.put.cs.ify.base.YTrigger;
-
-public class YTimerTrigger extends YTrigger {
-	public YTimerTrigger(int seconds){
-		//TODO: implement
-	}
-}
